e5ead94a0d703e3953f8f3407fe1b2f28b3ef0b6
uletin <dokterkoding111@gmail.com>
2024-03-07 14:13:10 +0700

    new

--- src/components/pages/user/BooksPage/index.jsx
+++ src/components/pages/user/BooksPage/index.jsx
@@ -2,18 +2,41 @@ import { Box, Fab, Typography } from "@mui/material"
 import PrimaryCard from "../../../atom/PrimaryCard"
 import { IconPencil } from "@tabler/icons-react"
 import { listBooks } from "../../../../_mock/listBooks"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 
 
 const BooksPage = () => {
 
-    console.log(listBooks)
+    const [books, setBooks] = useState([])
+    const urlBooks = "http://localhost:1337/api/books"   
+
+    useEffect(() => {
+        const getBooks = async () => {
+            try {
+                const getData = await axios.get(urlBooks)
+                setBooks(getData.data.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getBooks()
+
+    }, [])
+
+    console.log(books)
 
     return(
         <Box>
             <Box sx={{display:"flex", gap:2, justifyContent:"center"}}>
-                {listBooks.map((book) => (
-                    <PrimaryCard title={book.title} subtitle={book.subtitle} qty={book.qty} key={book.id} />
+                {books.map((book) => (
+                    <PrimaryCard 
+                        title={book.attributes.title} 
+                        subtitle={book.attributes.subtitle} 
+                        qty={book.attributes.qty} 
+                        key={book.id} 
+                    />
                 ))}
             </Box>         
             <Fab>
